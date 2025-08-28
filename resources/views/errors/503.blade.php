5e4e3644677ac0255792de6c5406862778f3e2b5
Sirius-G <garry.owen@users.noreply.github.com>
2025-08-28 08:40:30 +0100

    503 page update

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.loadingpage')
 
 @section('content')
-<div class="container text-center">
-    <div class="logo_text">Funk and Fable</div>
-    <div class="logo_sub_text mb-4">Professional Acoustic Band</div>
-    <div class="card mt-4">
-        <div class="card-header greenheader">
-            <strong class="text-white">{{ __('503 Error') }}</strong>
 
-            </div>
-            <div class="card-body bg-white">
-                <h1>We'll be back soon!</h1>
-                <br>
-                <p>Sorry for the inconvenience. We're performing some maintenance right now.<br>Please check back later.</p>
-            </div>  
-        </div>
-    <br><br><br>
+<div class="logocontainer text-center">
+    <div class="cover">
+      <h1 class="logo_text text">Funk & Fable</h1>
+      <p class="logo_sub_text_white sub_text">Professional Acoustic Band</p>
+<br><br><br>
+            <strong class="text-white">{{ __('503 Error') }}</strong>
+            <div class="inform_text">We'll be back soon!</div>
+            <br>
+            <p class="mx-4 inform_sub_text">Sorry for the inconvenience.
+            <br>We're performing some maintenance right now.<br>Please check back later.</p>
+    </div>
+
+    
 </div>
+
 @endsection
