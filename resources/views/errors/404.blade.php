7a3a11e02efbf71b26673b48bd319d934ac5ffaf
Sirius-G <garry.owen@users.noreply.github.com>
2025-09-03 08:29:58 +0100

    404 error update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,23 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.loadingpage')
 <?php
 session()->put('uri', $_SERVER['REQUEST_URI']);
 ?>
 
 @section('content')
-<div class="container text-center" style="margin-bottom: -200px;">
-    <div class="logo_text">Funk and Fable</div>
-    <div class="logo_sub_text mb-4">Professional Acoustic Band</div>
-    <div class="card mt-4">
-        <div class="card-header greenheader">
-            <strong class="text-white">{{ __('404 Error') }}</strong>
+<div class="logocontainer text-center">
+    <div class="cover">
+      <img src="images/svg/F&FLogoLinear.svg" class="logolinear" alt="Funk and Fable logo">
+      <img src="images/svg/F&FLogoStacked.svg" class="logostacked" alt="Funk and Fable logo">
+      <p class="logo_sub_text_white sub_text">Professional Acoustic Band</p>
+      <br><br>
+      <div class="container">
+        <div class="card mt-4">
+            <div class="card-header greenheader">
+                <strong class="text-white">{{ __('404 Error') }}</strong>
 
+                </div>
+                <div class="card-body bg-white">
+                    <h1>Page not found</h1>
+                    <br>
+                    <p>...redirecting, please wait!</p>
+                </div>  
             </div>
-            <div class="card-body bg-white">
-                <h1>Page not found</h1>
-                <br>
-                <p>...redirecting, please wait!</p>
-            </div>  
         </div>
+    </div>
+    </div>
 </div>
 <script>
     setTimeout(function(){
