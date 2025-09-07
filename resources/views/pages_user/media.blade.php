57670ac4d9d4de0937a8d6ea67ebf0db67b82578
Sirius-G <garry.owen@users.noreply.github.com>
2025-09-07 15:54:48 +0100

    Media page

---
+++ resources/views/pages_user/media.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="secondary-section wave-bottom"></section>
+    <div class="section_gap"></div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-5 py-4 my-4">
+            <h1 class="inform_text pt-4 mt-4"><u>Media</u></h1>
+            <p class="logo_sub_text">
+                Experience the sound of Beth and Connor beyond the stage. Here you’ll find our showreel that will give you a glimpse of what we bring to every event.
+                <br><br>
+                Watch & Listen to hear our unique sound of vocals and warm guitar tones. From stripped-back love songs to crowd-pleasing sing-alongs, our music is designed to set the perfect mood.
+                <br><br>
+                Stay connected with our latest music, gigs, and updates on social media. We regularly share live snippets and new covers so you can get a real taste of what we do.
+            </p>
+            <div class="text-center">
+                <img src="images/svg/divider.png" alt="Funk and Fable logo" width="100%">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-7 text-center py-4 my-4">
+            <img src="images/media.jpg" alt="Beth and Connor - Media page banner" width="100%" class="about_banner_mob">
+            <img src="images/media.jpg" alt="Beth and Connor - Media page banner" width="100%" class="about_banner">
+        </div>
+      </div>
+    </div>
+
+    <div class="section_gap"></div>
+    <section class="secondary-section wave-top"></section>
+
+    <div class="bg_primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 mb-4">
+                    <h2 class="inform_text">Acoustic Duo Showreel</h2>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="video-container">
+                                <iframe
+                                    src="https://www.youtube.com/embed/5aZT0oL3tvM" 
+                                    frameborder="0" 
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                    allowfullscreen>
+                                </iframe>
+                            <br>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                </div>        
+            </div>
+        </div>
+    </div>
+
+    <section class="secondary-section wave-bottom"></section>
+    <div class="section_gap"></div> 
+
+
+    </div>
+      
+@endsection
