b8c15400ee7731efbe0c1d74d3b9f42c8e504c79
Sirius-G <garry.owen@users.noreply.github.com>
2025-09-07 16:43:47 +0100

    contact page

--- resources/views/inc/contactform.blade.php
+++ resources/views/inc/contactform.blade.php
@@ -1,12 +1,34 @@
 <div class="contact_form">
     <p>All fields required</p>
     <form onsubmit="mailForm(this.name.value, this.email.value, this.message.value)">
-        <label for="name" class="label"><strong>Full Name: </strong></label><br>
-        <input tabindex="6" type="text" name="name" id="name" class="field item" placeholder="Please enter your full name" required><br>
-        <label for="email" class="label"><strong>Email Address: </strong></label><br>
-        <input tabindex="7" type="email" name="email" id="email" class="field item" placeholder="Please enter your email address" required><br>
-        <label for="msg" class="label"><strong>Message: </strong></label><br>
-        <textarea tabindex="8" name="message" id="msg" class="texta item" placeholder="Please enter your message" required data-gramm="false" data-gramm_editor="false" data-enable-grammarly="false"></textarea><br>
-        <input tabindex="9" type="submit" class="btn btn-primary btn-sm px-4 py-2 rounded-3 shadow-sm hover-button item" value="Send Message">
+        <fieldset class="border p-3 rounded">
+            <legend class="float-none w-auto px-2">Contact Details</legend>
+
+            <label for="name" class="label" style="margin-top: -35px;"><strong>Full Name: </strong></label><br>
+            <input tabindex="6" type="text" name="name" id="name" class="field item" placeholder="Please enter your full name" required><br>
+
+            <label for="email" class="label"><strong>Email Address: </strong></label><br>
+            <input tabindex="7" type="email" name="email" id="email" class="field item" placeholder="Please enter your email address" required><br>
+
+            <label for="phone" class="label"><strong>Phone Number: </strong></label><br>
+            <input tabindex="8" type="phone" name="phone" id="phone" class="field item" placeholder="Please enter your phone number" required><br>
+        </fieldset>
+        <br>
+        <fieldset class="border p-3 rounded">
+            <legend class="float-none w-auto px-2">Event Details</legend>
+
+            <label for="eventdate" class="label" style="margin-top: -35px;"><strong>Date: </strong></label><br>
+            <input tabindex="9" type="date" name="eventdate" id="eventdate" class="field item" required><br>
+
+            <label for="eventdate" class="label"><strong>Location: </strong></label><br>
+            <input tabindex="10" type="text" name="eventlocation" id="eventlocation" class="field item" placeholder="Please enter the event location (include venue name, street name and postal code)" required><br>
+
+            <label for="msg" class="label"><strong>Enquiry: </strong></label><br>
+            <textarea tabindex="11" name="message" id="msg" class="texta item" placeholder="Tell us more about your event..." required data-gramm="false" data-gramm_editor="false" data-enable-grammarly="false"></textarea><br>
+        </fieldset>
+
+        <div class="text-center">
+            <input tabindex="12" type="submit" class="btn btn-primary btn-lg px-4 py-2 rounded-5 shadow-sm hover-button item mt-4" value="Send Message">
+        </div>
     </form>
 </div>
---
+++ resources/views/pages_user/contact.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="secondary-section wave-bottom"></section>
+    <div class="section_gap"></div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-5 py-4 my-4">
+            <h1 class="inform_text pt-4 mt-4"><u>Contact us</u></h1>
+            <p class="logo_sub_text">
+                If you're interested in booking us for your event, please fill in the below form and we'll get back to you as soon as possible.
+            </p>
+            <div class="text-center">
+                <img src="images/svg/divider.png" alt="Funk and Fable logo" width="100%">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-7 text-center py-4 my-4">
+            <img src="images/contact.jpeg" alt="Beth and Connor - Contact page banner" width="100%" class="about_banner_mob">
+            <img src="images/contact.jpeg" alt="Beth and Connor - Contact page banner" width="100%" class="about_banner">
+        </div>
+      </div>
+    </div>
+
+    <div class="section_gap"></div>
+    <section class="secondary-section wave-top"></section>
+
+    <div class="bg_primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 mb-4">
+                    <h1 class="inform_text">Get in Touch</h1>
+                    <hr>
+                    <div class="row">
+                        @include('inc.contactform')
+                    </div>
+                    <hr>
+                </div>        
+            </div>
+        </div>
+    </div>
+
+    <section class="secondary-section wave-bottom"></section>
+    <div class="section_gap"></div> 
+
+
+    </div>
+      
+@endsection
