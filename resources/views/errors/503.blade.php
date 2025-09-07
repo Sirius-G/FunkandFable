a345ee8d769c9653419c0fea8908775eb84d3daa
Sirius-G <garry.owen@users.noreply.github.com>
2025-09-07 15:34:07 +0100

    503 error update

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -9,11 +9,11 @@
       <p class="logo_sub_text_white sub_text">Professional Acoustic Band</p>
       <br><br>
       <div class="container">
-        <div class="card mt-4">
-            <div class="card-header greenheader">
+        <div class="card mt-4 opacity-75">
+            <div class="card-header greenheader opacity-75">
                 <strong class="text-white">{{ __('503 Error') }}</strong>
                 </div>
-                <div class="card-body error_card">
+                <div class="card-body">
                     <h1>We'll be back soon!</h1>
                     <br>
                     <p>Sorry for the inconvenience.
