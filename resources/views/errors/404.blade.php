e5a1c7f3a69a6d84a96fd7e12571da12bd572d3b
Sirius-G <garry.owen@users.noreply.github.com>
2026-01-11 16:55:13 +0000

    404 error page update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,8 +6,8 @@ session()->put('uri', $_SERVER['REQUEST_URI']);
 @section('content')
 <div class="logocontainer text-center">
     <div class="cover">
-      <img src="images/svg/F&FLogoLinear.svg" class="logolinear" alt="Funk and Fable logo">
-      <img src="images/svg/F&FLogoStacked.svg" class="logostacked" alt="Funk and Fable logo">
+      <img src="images/svg/F&FLogoLinear.svg" class="logolinear"  style="margin-top: 150px;"  alt="Funk and Fable logo">
+      <img src="images/svg/F&FLogoStacked.svg" class="logostacked"  style="margin-top: 50px;"  alt="Funk and Fable logo">
       <p class="logo_sub_text_white sub_text">Professional Acoustic Band</p>
       <br><br>
       <div class="container">
